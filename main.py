4289fb38f901456050ee445a6b7a82ebe391d9be
Caio <caionogueirabindella@gmail.com>
2025-08-25 16:50:06 -0300

    Creating functions

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 from skimage import io
+from skimage import data
 import matplotlib.pyplot as plt
 import os
 
 # Importando as imagens
 img_dir = "assets"
 
-imagens = os.listdir(img_dir)
-
 # Loop para abrir e mostrar as imagens
-for nome in imagens:
-    caminho = os.path.join(img_dir, nome)
+def plot_all_images():
+    imagens = os.listdir(img_dir)
+    for nome in imagens:
+        fig, ax = plt.subplots( figsize=(8, 4))
+        
+        caminho = os.path.join(img_dir, nome)
+        # Lê a imagem com skimage
+        img = io.imread(caminho)
+        
+        ax.imshow(img)
+        ax.set_title(nome)
+        ax.axis("off") # Remove os eixos (números) da imagem
+        
+        # Plotando o histograma
+        fig.tight_layout()
+        plt.show()
+
+# Plot de uma imagem
+def plot_imagens(name):
+    # Criando uma figura com 1 linha e 2 colunas
+    fig, ax = plt.subplots(figsize=(8, 4))
     
-    # Lê a imagem com skimage
+    # Obtendo o caminho da imagem
+    caminho = os.path.join(img_dir, name)
     img = io.imread(caminho)
     
-    # Plota
-    plt.figure(figsize=(5, 5))
-    plt.imshow(img)
-    plt.title(nome)
-    plt.axis("off")
-    plt.show()
+    # Plotando a imagem
+    ax.imshow(img)
+    ax.set_title(name)
+    ax.axis("off") # Remove os eixos (números) da imagem
     
-# Plot de imagem específica
-# img = io.imread(os.path.join(img_dir, "flor_o_n_14.jpg"))
-# plt.imshow(img)
-# plt.axis("off")
-# plt.show()
+    # Plotando o histograma
+    fig.tight_layout()
+    plt.show()
+
+# Chamndo a função para plotar apenas uma imagem
+# plot_imagens("carro_o_n_14.jpg")
+
+# Chamndo a função para plotar todas as imagens
+plot_all_images()
